af9e21d495f56216bad607b62a4f8e073c57da90
Cristian M <crisfemoar2011@hotmail.com>
2021-09-02 15:53:15 -0500

    exercise ready

--- test.py
+++ test.py
@@ -1,4 +1,72 @@
-names = ["Cristian", "Valeria", "Santiago"]
+emails = []
 
-for name in names:
-    print("Buenas tardes, %s" % name)
+# Cristian
+emails.append("crisfemoar2011@hotmail.com")
+
+
+# Nini
+
+
+
+# Julian
+
+
+
+# Jonathan Gonzalez
+
+
+
+# Juan David
+
+
+
+# Mary Ann
+
+
+
+# Mayra
+
+
+
+# Juan Pablo
+
+
+
+# Lady
+
+
+
+# Stephanie
+
+
+
+# Wilson
+
+
+
+# Diego
+
+
+
+# Monica
+
+
+
+# Jhonatan Andres
+
+
+
+#----------------------------
+dic = dict()
+
+with open("mails.txt", "r") as f:
+    for line in f.readlines():
+        mail, name = line.split(",")
+        dic[mail] = name
+
+for mail in emails:
+    if mail in dic:
+        print("El email %s es de %s" %(mail, dic[mail]) )
+    else:
+        print("El email %s no está en la base de datos" % mail)
+
