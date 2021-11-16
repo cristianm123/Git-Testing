6cfe69a788954e6601176c8fa7f94f17e2dada86
Cristian M <crisfemoar2011@hotmail.com>
2021-11-16 15:38:01 -0500

    p

--- test.py
+++ test.py
@@ -17,7 +17,6 @@ from tools.Monica import addMonicaToParty
 from tools.Jhonatan import addJhonatanToParty
 from tools.Gamarra import addGamarraToParty
 
-party = ["Valeria novia linda"]
 party = addNiniToParty(party)
 party = addJulianToParty(party)
 party = addJonathanToParty(party)
